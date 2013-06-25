1cfecfb4451f56c614225e7cd8f894deed63e0c1
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-06-25 12:47:02 -0700

    cookies rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CookiesUzma::Application.config.secret_token = 'fab7f76ef4e212795a4b2a988975e127b04a4be42fe246f228d4d30ab6c8078cbd5841bf2dd636cfac0fba1120840322aea0377e2cf0ccf9fc4443888622921c'
